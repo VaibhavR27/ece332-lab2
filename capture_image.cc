c54060873d0c7a4dfabe0819ef707b4691668f07
VaibhavR27 <63654753+VaibhavR27@users.noreply.github.com>
2025-04-16 22:28:53 -0400

    final files
    working files for submission. shown to work in lab demo.

--- capture_image.cc
+++ capture_image.cc
@@ -4,7 +4,6 @@
 #include <sys/mman.h>
 #include <stdint.h>
 #include "bmp_utility.h"
-
 #include <unistd.h>   // For close(), read(), write()
 using ::close;
 using ::open;
@@ -29,11 +28,11 @@ using ::open;
 #define SW_BASE 0xFF200040
 #define STRIDE 512
 
-// TODO: test
+
 int main(void) {
     volatile unsigned int *key_ptr = NULL;
     volatile unsigned int *video_in_dma = NULL;
-    volatile unsigned short *video_mem = NULL;
+    volatile unsigned char *video_mem = NULL;
     volatile unsigned int *sw_ptr = NULL;
     void *virtual_base;
     int fd;
@@ -55,8 +54,8 @@ int main(void) {
 
     // Calculate the virtual address where our device is mapped
     video_in_dma = (unsigned int *)((uint8_t *)virtual_base + ((VIDEO_BASE) & (HW_REGS_MASK)));
-    key_ptr = (unsigned int *)((uint8_t *)virtual_base + ((KEY_BASE) & (HW_REGS_MASK)));
-    video_mem = (unsigned short *)((uint8_t *)virtual_base + ((FPGA_ONCHIP_BASE) & (HW_REGS_MASK)));
+    key_ptr = (unsigned int *)((uint8_t *)virtual_base + ((PUSH_BASE) & (HW_REGS_MASK)));
+    video_mem = (unsigned char *)((uint8_t *)virtual_base + ((FPGA_ONCHIP_BASE) & (HW_REGS_MASK)));
     sw_ptr = (unsigned int *)((uint8_t *)virtual_base + ((SW_BASE) & (HW_REGS_MASK)));
 
 
@@ -72,28 +71,42 @@ int main(void) {
 
     printf("enabled video:0x%x\n",value);
 
-    while(1){
-        // key other than key0 is changed
-        if ((*key_ptr & 0xE) != 0xE) {
-                *(video_in_dma+3) = 0x0;
-                while ( (*key_ptr & 0xE) != 0xE );
-                image_count++;
+	printf("KEY value: 0x%x\n", *key_ptr);
+	fflush(stdout);
+   	unsigned short image_buffer[IMAGE_HEIGHT][IMAGE_WIDTH];
 
-                char filename_color[64];  
+
+	while(1){
+        // key other than key0 is changed
+        if ((*key_ptr) != 7) {
+		printf("paused video\n");
+                *(video_in_dma+3) = 0x0;
+                while ( (*key_ptr) != 7 );
+                image_count++;
+		// Copy frame into local buffer
+        for (int y = 0; y < IMAGE_HEIGHT; y++) {
+            for (int x = 0; x < IMAGE_WIDTH; x++) {
+                int offset = (y << 9) + (x);
+                image_buffer[y][x] = *(volatile unsigned short *)(video_mem + offset);
+            }
+        }
+
+                printf("captured image\n");
+		char filename_color[64];  
                 sprintf(filename_color, "final_image_color_%d.bmp", image_count);
                 char filename_bw[64];  
                 sprintf(filename_bw, "final_image_bw_%d.bmp", image_count);
 
-                saveImageShort(filename_color, (unsigned short *)video_mem,IMAGE_WIDTH,IMAGE_HEIGHT);
-                saveImageGrayscale(filename_bw,(const unsigned char *)video_mem,IMAGE_WIDTH,IMAGE_HEIGHT);
+                saveImageShort(filename_color, &image_buffer[0][0],IMAGE_WIDTH,IMAGE_HEIGHT);
+                saveImageGrayscale(filename_bw,(const unsigned char *)&image_buffer[0][0],IMAGE_WIDTH,IMAGE_HEIGHT);
 
-                if ((*key_ptr & 0xE) != 0xE) {
+                if ((*key_ptr) == 7) {
                         *(video_in_dma + 3) = 0x0; 
                         *(video_in_dma + 3) = 0x4; 
-                }
+                	printf("resumed video\n");
+		}
         }
     }
-
     // Clean up
     if (munmap(virtual_base, IMAGE_SPAN) != 0) {
         printf("ERROR: munmap() failed...\n");
